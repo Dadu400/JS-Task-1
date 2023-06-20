e7facce1ad540c07afc38d92a9f7ee035a986abf
Dali Khukhunashvili <daduka.xuxunashvili@gmail.com>
2023-06-20 16:02:16 +0400

    comment

--- index.js
+++ index.js
@@ -1,44 +1,44 @@
-let month = "july";
+// let month = "july";
 
 
-switch (month) {
-    case "january":
-    case "february":
-    case "december":
-    console.log("winter")
-    break;
-    case "march":
-    case "april":
-    case "may":
-    console.log("spring")
-    break;
-    case "june":
-    case "july":
-    case "august":
-    console.log("summer")
-    break;
-    case "september":
-    case "october":
-    case "november":
-    console.log("autumn")
-    break;  
-}
+// switch (month) {
+//     case "january":
+//     case "february":
+//     case "december":
+//     console.log("winter")
+//     break;
+//     case "march":
+//     case "april":
+//     case "may":
+//     console.log("spring")
+//     break;
+//     case "june":
+//     case "july":
+//     case "august":
+//     console.log("summer")
+//     break;
+//     case "september":
+//     case "october":
+//     case "november":
+//     console.log("autumn")
+//     break;  
+// }
 
-let a = 20;
+// let a = 20;
 
-let b = 21;
+// let b = 21;
 
-let c = 8;
+// let c = 8;
 
-switch (true) {
-    case (a >= b && a >= c):
-        console.log(`max is ${a}`)
-        break;
-        case (b >= a && b >= c):
-        console.log(`max is ${b}`)
-        break;
-        case (c >= a && c >= b):
-        console.log(`max is ${c}`)
-        break;
-}
+// switch (true) {
+//     case (a >= b && a >= c):
+//         console.log(`max is ${a}`)
+//         break;
+//         case (b >= a && b >= c):
+//         console.log(`max is ${b}`)
+//         break;
+//         case (c >= a && c >= b):
+//         console.log(`max is ${c}`)
+//         break;
+// }
  
